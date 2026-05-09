c5441e024cc29de6f0305c12aab2eb6c4ea50aa3
brunophg <brunophgjf@gmail.com>
2026-05-09 18:11:19 -0300

    Project created

---
+++ src/main/java/com/brunophg/workshop_springboot_jpa/WorkshopSpringbootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.brunophg.workshop_springboot_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringbootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringbootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brunophg/workshop_springboot_jpa/WorkshopSpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunophg.workshop_springboot_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringbootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
