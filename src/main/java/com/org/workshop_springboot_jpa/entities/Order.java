a5ab9ac003cc03d13181bc9b4513b6771793e2b3
brunophg <brunophgjf@gmail.com>
2026-05-11 08:37:05 -0300

    OrderStatus enum

--- src/main/java/com/org/workshop_springboot_jpa/entities/Order.java
+++ src/main/java/com/org/workshop_springboot_jpa/entities/Order.java
@@ -2,6 +2,7 @@ package com.org.workshop_springboot_jpa.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.org.workshop_springboot_jpa.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -21,6 +22,9 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd 'T' HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -28,10 +32,11 @@ public class Order implements Serializable {
     public Order() {
 
     }
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, User client, OrderStatus orderStatus) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
     }
 
     public Long getId() {
@@ -56,6 +61,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
